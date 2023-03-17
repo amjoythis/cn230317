970fac031c76ecf0eee4c7d1f937c5248646dcc5
Artur Marques <am@arturmarques.com>
2023-03-17 12:15:28 +0000

    1st working version of the CLI multiplier

--- multiplier.py
+++ multiplier.py
@@ -1,2 +1,22 @@
 # multiplier.py
+import sys
 
+def multiplier(pList:list):
+    result = 1
+    for n in pList:
+        result*=float(n)
+    # for
+    return result
+# def multiplier
+
+if(len(sys.argv)>1): # check if there are args
+    the_values:list = sys.argv[1:] # except the script name
+    the_mult = multiplier(the_values)
+    the_output =\
+    f"The mult of {the_values} is {the_mult}"
+    print(the_output)
+# if
+else:
+    the_output = "Please provide some numbers."
+    print(the_output)
+# if-else
